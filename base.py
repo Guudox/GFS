d2a366be559239ee6fafad85f3de4fa7593fa5be
Michael Kiernan <michael@mkiernan.com>
2022-01-18 15:00:49 -0500

    Added Forms and SFTP

--- base.py
+++ base.py
@@ -1,13 +1,12 @@
 # import asyncio
-import window
+from window import GuuFileSync
+
+GuuFileSync()
+
 # from check_process import check_if_game_is_running
 # from client import Client
-
 # async def process_corotine():
 #     while True:
 #         check_if_game_is_running('discord') #'Maine-Win64-Shipping'
 #         await asyncio.sleep(5)
 # Client.loop(process_corotine())
-
-
-window
